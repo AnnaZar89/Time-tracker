286d684ad49b2c71d9f2a295692531aad7e89257
Anna Zaryczanska <annazar00@gmail.com>
2022-03-13 07:38:23 +0100

    adding date-fns and firebase

--- script.js
+++ script.js
@@ -11,6 +11,47 @@ let labelInResults = document.querySelector('.results .label');
 let checkbox = document.querySelector('.switch input');
 let buttonReset = document.querySelector('.reset');
 
+let sumOfProductionValues = 0;
+let sumOfNPTValues = 0;
+let sumOfTrainingValues = 0;
+let sumOfBreakValues = 0;
+
+
+let workDayConverted = [];
+let arrayOfConvertedProductionValues = [];
+let arrayOfConvertedNPTValues = [];
+let arrayOfConvertedTrainingValues = [];
+let arrayOfConvertedBreakValues = [];
+
+let sumOfworkDayConverted = [];
+let arrayOfConvertedSumOfProductionValues = [];
+let arrayOfConvertedSumOfNPTValues = [];
+let arrayOfConvertedSumOfTrainingValues = [];
+let arrayOfConvertedSumOfBreakValues = [];
+
+const weeklyResultsDiv = document.querySelector('.weeklyResultsDiv');
+const actualResults = document.querySelector('.actualResults');
+let weekSum = {};
+
+
+
+
+/*FIREBASE*/
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBVA4ajZsSa5EVIJtiYqNrkqPP80Y2QMHQ",
+  authDomain: "test-b8ead.firebaseapp.com",
+  projectId: "test-b8ead",
+  storageBucket: "test-b8ead.appspot.com",
+  messagingSenderId: "500859864296",
+  appId: "1:500859864296:web:2914a519860cfc9b38355d",
+  measurementId: "G-DNZF9TEG42"
+
+});
+
+
+var db = firebase.firestore();
+
 
 
 /*stopwatch*/
@@ -42,10 +83,6 @@ function getProductionTime() {
   let productionMinutesRounded = Math.round(productionMinutes);
 
 
-  let sumOfProductionValues = 0;
-  let sumOfNPTValues = 0;
-  let sumOfTrainingValues = 0;
-  let sumOfBreakValues = 0;
 
 
 
@@ -88,6 +125,7 @@ function updatedResults() {
     labelInResults.innerHTML = 'yours results today:';
   } else {
     labelInResults.innerHTML = 'yours weekly results:';
+
   }
 }
 
@@ -136,7 +174,6 @@ function displayTime(hour, minutes, seconds) {
 let arrayWithStatusFiltered = [];
 
 function updateResults() {
-  //  clearInterval(stopwatch.intervalId);
 
   state.results.push({
     time: stopwatch.elapsedTime,
@@ -147,45 +184,49 @@ function updateResults() {
   state.currentStatus = list.value;
 
 
-  let getArrayOfProductionResults = state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Production');
-  let getArrayOfNPTResults = state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Non-productive time');
-  let getArrayOfTrainingResults = state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Training');
-  let getArrayOfBreakResults = state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Break');
 
-  console.log(getArrayOfProductionResults);
+
+  let getArrayOfProductionResults = filterByStatus('Production');
+  let getArrayOfNPTResults = filterByStatus('Non-productive time');
+  let getArrayOfTrainingResults = filterByStatus('Training');
+  let getArrayOfBreakResults = filterByStatus('Break');
+
+
 
 
   let getArrayOfProductionValues = getTimestamp(getArrayOfProductionResults);
-  console.log(getArrayOfProductionValues);
   sumOfProductionValues = sumOfStatusTime(getArrayOfProductionValues);
-
-  let arrayOfConvertedProductionValues = convertion(sumOfProductionValues);
+  arrayOfConvertedProductionValues = convertion(sumOfProductionValues);
   document.getElementsByClassName('givenStatusTime')[1].innerHTML = `${arrayOfConvertedProductionValues[0]} h ${arrayOfConvertedProductionValues[1]} min ${arrayOfConvertedProductionValues[2]} sec`;
 
 
   let getArrayOfNPTValues = getTimestamp(getArrayOfNPTResults);
   sumOfNPTValues = sumOfStatusTime(getArrayOfNPTValues);
-  let arrayOfConvertedNPTValues = convertion(sumOfNPTValues);
+  arrayOfConvertedNPTValues = convertion(sumOfNPTValues);
   document.getElementsByClassName('givenStatusTime')[2].innerHTML = `${arrayOfConvertedNPTValues[0]} h ${arrayOfConvertedNPTValues[1]} min ${arrayOfConvertedNPTValues[2]} sec`;
 
 
 
   let getArrayOfTrainingValues = getTimestamp(getArrayOfTrainingResults);
   sumOfTrainingValues = sumOfStatusTime(getArrayOfTrainingValues);
-  //      console.log(sumOfTrainingValues);
-  let arrayOfConvertedTrainingValues = convertion(sumOfTrainingValues);
+  arrayOfConvertedTrainingValues = convertion(sumOfTrainingValues);
   document.getElementsByClassName('givenStatusTime')[3].innerHTML = `${arrayOfConvertedTrainingValues[0]} h ${arrayOfConvertedTrainingValues[1]} min ${arrayOfConvertedTrainingValues[2]} sec`;
+
+
 
   let getArrayOfBreakValues = getTimestamp(getArrayOfBreakResults);
   sumOfBreakValues = sumOfStatusTime(getArrayOfBreakValues);
-  let arrayOfConvertedBreakValues = convertion(sumOfBreakValues);
-
+  arrayOfConvertedBreakValues = convertion(sumOfBreakValues);
   document.getElementsByClassName('givenStatusTime')[4].innerHTML = `${arrayOfConvertedBreakValues[0]} h ${arrayOfConvertedBreakValues[1]} min ${arrayOfConvertedBreakValues[2]} sec`;
 
 
   let workDay = sumOfProductionValues + sumOfNPTValues + sumOfTrainingValues + sumOfBreakValues;
-  let workDayConverted = convertion(workDay);
+  workDayConverted = convertion(workDay);
   document.getElementsByClassName('givenStatusTime')[0].innerHTML = `${workDayConverted[0]} h ${workDayConverted[1]} min  ${workDayConverted[2]} sec`;
+
+
+
+
 
 
 
@@ -193,15 +234,12 @@ function updateResults() {
 
   if (list.value !== 'Offline') {
     startStopWatch();
-  }
-  if (list.value === 'Offline') {
-    clearInterval(stopwatch.intervalId);
-    displayTime(0, 0, 0);
 
   }
+
   if (list.value === 'Offline' && checkbox.checked != true) {
     state.results = [];
-    //        displayTime(0, 0, 0);
+
     document.getElementsByClassName('givenStatusTime')[0].innerHTML = '00 h 00 min 00 sec';
     document.getElementsByClassName('givenStatusTime')[1].innerHTML = '00 h 00 min 00 sec';
     document.getElementsByClassName('givenStatusTime')[2].innerHTML = '00 h 00 min 00 sec';
@@ -210,46 +248,166 @@ function updateResults() {
 
   }
 
-  if (list.value === 'Offline' && checkbox.checked != false) {
 
+  if (checkbox.checked != false) {
+    state.results = [];
   }
+
+
+
+  if (list.value === 'Offline') {
+
+    clearInterval(stopwatch.intervalId);
+    displayTime(0, 0, 0);
+
+
+    db.collection(firstDayOfWeek()).add({
+
+        production: sumOfProductionValues,
+        NPT: sumOfNPTValues,
+        Training: sumOfTrainingValues,
+        Break: sumOfBreakValues
+
+
+      })
+
+
+      .then(() => {
+        downloadWeeklyResults();
+      })
+
+
+
+  };
 
 
 };
 
 
+function downloadWeeklyResults() {
+
+  db.collection(firstDayOfWeek()).onSnapshot((snapshot) => {
+
+    let history = [];
+
+    snapshot.docs.map(doc => {
+
+      history.push(doc.data());
+
+    })
+
+
+    console.log(history);
+    weekSum = history.reduce(function (previousValue, currentValue) {
+      return {
+        production: previousValue.production + currentValue.production,
+        NPT: previousValue.NPT + currentValue.NPT,
+        Training: previousValue.Training + currentValue.Training,
+        Break: previousValue.Break + currentValue.Break
+      }
+    }, {
+      production: 0,
+      NPT: 0,
+      Training: 0,
+      Break: 0
+    })
+
+
+    let sumOfworkDay = weekSum.production + weekSum.NPT + weekSum.Training + weekSum.Break;
+    sumOfworkDayConverted = convertion(sumOfworkDay);
+    document.getElementsByClassName('givenStatusTimeWeekly')[0].innerHTML = `${sumOfworkDayConverted[0]} h ${sumOfworkDayConverted[1]} min  ${sumOfworkDayConverted[2]} sec`;
+
+
+    arrayOfConvertedSumOfProductionValues = convertion(weekSum.production);
+    arrayOfConvertedSumOfNPTValues = convertion(weekSum.NPT);
+    arrayOfConvertedSumOfTrainingValues = convertion(weekSum.Training);
+    arrayOfConvertedSumOfBreakValues = convertion(weekSum.Break);
+
+    document.getElementsByClassName('givenStatusTimeWeekly')[1].innerHTML = `${arrayOfConvertedSumOfProductionValues[0]} h ${arrayOfConvertedSumOfProductionValues[1]} min ${arrayOfConvertedSumOfProductionValues[2]} sec`;
+    document.getElementsByClassName('givenStatusTimeWeekly')[2].innerHTML = `${arrayOfConvertedSumOfNPTValues[0]} h ${arrayOfConvertedSumOfNPTValues[1]} min ${arrayOfConvertedSumOfNPTValues[2]} sec`;
+
+    document.getElementsByClassName('givenStatusTimeWeekly')[3].innerHTML = `${arrayOfConvertedSumOfTrainingValues[0]} h ${arrayOfConvertedSumOfTrainingValues[1]} min ${arrayOfConvertedSumOfTrainingValues[2]} sec`;
+
+    document.getElementsByClassName('givenStatusTimeWeekly')[4].innerHTML = `${arrayOfConvertedSumOfBreakValues[0]} h ${arrayOfConvertedSumOfBreakValues[1]} min ${arrayOfConvertedSumOfBreakValues[2]} sec`;
+
+
+  });
+
+  ifOffline();
+};
 
 
 
+function firstDayOfWeek() {
+  const startOfWeek = dateFns.startOfWeek;
+  let firstDay = startOfWeek(new Date(), {
+    weekStartsOn: 1
+  });
+
+  firstDay = dateFns.format(firstDay, 'DD.MM.YYYY');
+  console.log(firstDay);
+
+  return firstDay
+}
+
+
+firstDayOfWeek();
 
 function convertion(milliseconds) {
   let arrayOfHoursMinutsSeconds = [];
   const sec = parseInt((milliseconds / 1000) % 60);
   const min = parseInt((milliseconds / (1000 * 60)) % 60);
   const hou = parseInt((milliseconds / (1000 * 60 * 60)) % 24);
+
   arrayOfHoursMinutsSeconds.push(hou, min, sec);
-  const pushedArrayOfHoursMinutsSeconds = arrayOfHoursMinutsSeconds.map(time => time < 10 ? `0${time}` : time)
+
+
+  const pushedArrayOfHoursMinutsSeconds = arrayOfHoursMinutsSeconds.map(function (time) {
+    if (time < 10) {
+      return '0' + time;
+
+    } else {
+      return time;
+    }
+
+
+  });
 
   return pushedArrayOfHoursMinutsSeconds;
+
 }
+
+
 
 function updateWeeklyResults() {
+
   if (checkbox.checked == true) {
-
-    //    updateResults();
-
+    weeklyResultsDiv.style.display = 'block';
+    actualResults.style.display = 'none';
   }
 
+  if (checkbox.checked != true) {
+    weeklyResultsDiv.style.display = 'none';
+    actualResults.style.display = 'block';
+  }
 };
 
-
 function filterByStatus(status) {
-  return state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Production');
+  if (status === 'Production') {
+    return state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Production');
+  }
+  if (status === 'Non-productive time') {
+    return state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Non-productive time');
+  }
+  if (status === 'Training') {
+    return state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Training');
+  }
+  if (status === 'Break') {
+    return state.results.filter(arrayWithStatus => arrayWithStatus.status === 'Break');
+  }
 }
 
-//console.log(window.dateFns);
 
-//console.log(window.dateFns.format(new Date(), 'DD-MM-YYYY'));
 
 
 function getTimestamp(array) {
@@ -259,6 +417,22 @@ function getTimestamp(array) {
 function sumOfStatusTime(arrayOfTimes) {
   return arrayOfTimes.reduce((acc, value) => acc + value, 0);
 }
+
+
+function ifOffline() {
+  if (list.value === 'Offline' && checkbox.checked != true) {
+    state.results = [];
+
+    document.getElementsByClassName('givenStatusTime')[0].innerHTML = '00 h 00 min 00 sec';
+    document.getElementsByClassName('givenStatusTime')[1].innerHTML = '00 h 00 min 00 sec';
+    document.getElementsByClassName('givenStatusTime')[2].innerHTML = '00 h 00 min 00 sec';
+    document.getElementsByClassName('givenStatusTime')[3].innerHTML = '00 h 00 min 00 sec';
+    document.getElementsByClassName('givenStatusTime')[4].innerHTML = '00 h 00 min 00 sec';
+
+  }
+}
+
+
 
 workDayHours.addEventListener('input', getProductionTime)
 workDayMinutes.addEventListener('input', getProductionTime)
@@ -270,7 +444,6 @@ hoursOfProduction.addEventListener('input', changingProductionTime)
 minutesOfProduction.addEventListener('input', changingProductionTime)
 checkbox.addEventListener('click', updatedResults);
 buttonReset.addEventListener('click', reset);
-
-//list.addEventListener('change', updateResults);
 list.addEventListener('change', updateResults);
 checkbox.addEventListener('click', updateWeeklyResults);
+checkbox.addEventListener('click', ifOffline);
